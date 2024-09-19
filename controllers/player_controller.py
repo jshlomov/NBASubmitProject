cccad30f406c36a7a6df764b5169f63f6c0475d8
jshlomov <jonatanshv@gmail.com>
2024-09-19 17:57:40 +0300

    final

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,16 +1,14 @@
 from dataclasses import asdict
-from http.cookiejar import debug
-from idlelib.debugger_r import debugging
 
 import services.player_service as s
 from flask import Blueprint, jsonify, request
-
-from repository.Player_repository import get_all_players
 
 player_blueprint = Blueprint("player", __name__)
 
 @player_blueprint.route("/", methods=['GET'])
 def get_all_players():
     args = request.args
-    players = list(map(asdict, s.get_all_players(args.get("position"), args.get("season"))))
-    return jsonify(players), 200
+    players = s.get_all_players(args.get("position"), args.get("season"))
+    breakpoint()
+    players_to_return = list(map(asdict, players))
+    return jsonify(players_to_return), 200
