ddd2b5d80db02ba6c0a6b7460f13274866caf9ee
jshlomov <jonatanshv@gmail.com>
2024-09-19 11:13:56 +0300

    details per season model

--- repository/database.py
+++ repository/database.py
@@ -1,4 +1,18 @@
-d
+from config.config_sql import SQL_URI
+from psycopg2.extras import RealDictCursor
 
-ef get_db_connection():
+
+def get_db_connection(psycopg2=None):
     return psycopg2.connect(SQL_URI, cursor_factory=RealDictCursor)
+
+def create_players_table():
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS players (
+                    id SERIAL PRIMARY KEY,
+                    question_text VARCHAR(255) NOT NULL,
+                    correct_answer VARCHAR(255) NOT NULL
+                )
+            ''')
+            connection.commit()
